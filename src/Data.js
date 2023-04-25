e17146ee5878200874e03f2e4c6b986515a6c82a
dana younis <younisdana7@gmail.com>
2023-04-25 15:51:48 +0000

    1

---
+++ src/Data.js
@@ -0,0 +1,33 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "fresh orange",
+    price: 5,
+    cat:'vegetables and fruits',
+    cat:'Fresh vegetables and fruits in cheap price',
+    img: "image/product-1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "fresh onion",
+    price: 2,
+    cat:'vegetables and fruits',
+    cat:'Fresh vegetables and fruits in cheap price',
+    img: "image/product-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "fresh meat",
+    price: 20,
+    img:"image/product-3.png",
+    cat:'fresh meat',
+  },
+  {
+    id: "thyfhcbcv",
+    name: "fresh cabbage",
+    price: 3,
+    cat:'vegetables and fruits',
+    cat:'Fresh vegetables and fruits in cheap price',    img: "image/product-4.png",
+  },
+];
+ 
